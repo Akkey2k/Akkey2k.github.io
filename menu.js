1eebd4c8dbd996927c713576b3f48804757e64e4
Akkey2k <38500118+Akkey2k@users.noreply.github.com>
2019-09-24 16:56:36 +0300

    scripts for animation
    scripts for animation

---
+++ menu.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+    // открыть/закрыть
+    $("#open_menu").click(function(){
+        $("#menu_list").fadeIn("slow");
+    });
+
+    $("#close_menu").click(function(){
+        $("#menu_list").fadeOut("slow");
+    });
+    // скрол
+    $("#services").click(function (){
+        $('html, body').animate({
+            scrollTop: $(".wrapper_service").offset().top
+        }, 1500);
+        $("#menu_list").fadeOut("slow");
+    });
+    $("#contacts").click(function (){
+        $('html, body').animate({
+            scrollTop: $(".wrapper_contact").offset().top
+        }, 1500);
+        $("#menu_list").fadeOut("slow");
+    });
+    $("#goto").click(function (){
+        $('html, body').animate({
+            scrollTop: $("header").offset().top
+        }, 1000);
+    });
+    // goto up
+    $(window).scroll(function(){
+        if($(window).scrollTop()>300){
+            $('#goto').fadeIn("slow")
+        }
+    })
+    $(window).scroll(function(){
+        if($(window).scrollTop()<300){
+            $('#goto').fadeOut("slow")
+        }
+    })
+});  
