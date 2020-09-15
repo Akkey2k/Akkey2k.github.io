411301a92f4f995e802f56dfecc32af8253c980f
Akkey2k <38500118+Akkey2k@users.noreply.github.com>
2020-09-15 16:45:21 +0300

    fix mobile hover by double touch

---
+++ assets/js/onHover.js
@@ -0,0 +1,17 @@
+const touchHover = function() {
+    document.querySelectorAll('[data-hover]').forEach(project => {
+        project.addEventListener('click', function (e) {
+            e.preventDefault();
+            const $this = e.target;
+            // const onHover = $this.getAttribute('data-hover');
+            const linkHref = $this.closest('a').getAttribute('href');
+            if (linkHref && $this.classList.contains('onHover')) {
+                location.href = linkHref;
+                return false;
+            }
+            $this.classList.toggle('onHover');
+        });
+    });
+};
+
+touchHover()
