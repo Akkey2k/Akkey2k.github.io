ff3c40169a1f07755046d4c6713b1a729a3b25c6
Akkey2k <38500118+Akkey2k@users.noreply.github.com>
2020-09-16 14:47:36 +0300

    fix hover mobile

--- assets/js/onHover.js
+++ assets/js/onHover.js
@@ -1,17 +1,50 @@
+// const touchHover = function() {
+//     const projects = document.querySelectorAll('[data-hover]').forEach(project => {
+//         console.log(project);
+//         const p = new Map(projects)
+//         // for(let i = 0;i<projects.length;i++){
+//         //     projects[i].classList.remove('onHover');
+//         // }
+//         project.addEventListener('click', function (e) {
+//             e.preventDefault();
+//             const $this = e.target;
+//             const linkHref = $this.closest('a').getAttribute('href');
+//             if ($this && linkHref && $this.classList.contains('onHover')) {
+//                 // location.href = linkHref;
+//                 window.open(linkHref, '_blank');
+//                 return false;
+//             }
+//             $this.classList.add('onHover');
+//         });
+//     });
+// };
+//
+// touchHover()
+
+
+
+
 const touchHover = function() {
-    document.querySelectorAll('[data-hover]').forEach(project => {
-        project.addEventListener('touchend', function (e) {
+    const projects = document.querySelectorAll('[data-hover]');
+    for(let i = 0;i<projects.length;i++){
+        projects[i].addEventListener('touchend', function (e) {
+            //Для таргета
             e.preventDefault();
             const $this = e.target;
-            // const onHover = $this.getAttribute('data-hover');
             const linkHref = $this.closest('a').getAttribute('href');
-            if (linkHref && $this.classList.contains('onHover')) {
-                location.href = linkHref;
+            if ($this && linkHref && $this.classList.contains('onHover')) {
+                // location.href = linkHref;
+                window.open(linkHref, '_blank');
                 return false;
             }
-            $this.classList.toggle('onHover');
+            //Перебор всех элементов и ремув класса на них
+            projects.forEach(project => {
+                project.classList.remove('onHover');
+            });
+            $this.classList.add('onHover');
         });
-    });
+    }
+
 };
 
 touchHover()
