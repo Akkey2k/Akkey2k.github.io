9d18692eb4945d866d58e5b47e3a62f3db7816a0
Akkey2k <38500118+Akkey2k@users.noreply.github.com>
2021-04-26 13:12:15 +0300

    remove old trash projects

--- emphasoft/assets/js/main.js
+++
@@ -1,6 +0,0 @@
-const menu = document.querySelector('.burger');
-const header_mobile = document.querySelector('.nav-mobile');
-
-menu.addEventListener('click', function (){
-    header_mobile.classList.toggle('open')
-});
